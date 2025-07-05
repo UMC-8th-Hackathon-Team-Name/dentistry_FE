17d62e8f1195643eb58b7787affb198fdac8889e
2ssac <ivory.ma9ic@gmail.com>
2025-07-05 20:52:48 +0900

    design:minor change on loginbutton

--- app/src/components/LoginButton.tsx
+++ app/src/components/LoginButton.tsx
@@ -5,11 +5,11 @@ interface LoginbuttonProps {
 const LoginButton = ({ onClick }: LoginbuttonProps) => {
   return (
     <button
-      role="로그인 "
+      role="로그인 버튼"
       onClick={onClick}
       className="w-[325px] h-[52px] rounded-[10px] text-[20px] bg-[#FFFFFF] text-[#465785]"
     >
-      로그인 버튼
+      로그인 하기
     </button>
   );
 };
