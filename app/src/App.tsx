68569ffdcee0d893625591a0b7550a24ac5d62c0
2ssac <ivory.ma9ic@gmail.com>
2025-07-06 02:40:00 +0900

    App.tsx 오타 수정

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
-import LandigPage from "./auth/LandingPage";
+// auth
+import LandingPage from "./auth/LandingPage";
 import SignupTerms from "./auth/SignupTerms";
-import Main from "./main/Main";
 import SignupEmail from "./auth/SignupEmail";
 import SignupPassword from "./auth/SignupPassword";
 import SignupComplete from "./auth/SignupComplete";
 import Login from "./auth/Login";
 import DeleteAccount from "./auth/DeleteAccount";
 
+// pages
+import Main from "./main/Main";
 import DetailPage from "./pages/DetailPage";
 import SettingPage from "./pages/SettingPage";
 import FixPage from "./pages/FixPage";
@@ -19,16 +21,18 @@ function App() {
     <div className="m-auto max-h-[912px] max-w-[412px] bg-[#FBFBFB] border-2 border-black">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandigPage />} />
-          <Route path="signup/terms" element={<SignupTerms />} />
-          <Route path="/main" element={<Main />} />
+          {/* 인증/회원가입 관련 */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup/terms" element={<SignupTerms />} />
           <Route path="/signup/email" element={<SignupEmail />} />
           <Route path="/signup/password" element={<SignupPassword />} />
           <Route path="/signup/complete" element={<SignupComplete />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/delete-account" element={<DeleteAccount />} />
 
-          <Route path="/" element={<DetailPage />} />
+          {/* 메인 및 기타 페이지 */}
+          <Route path="/main" element={<Main />} />
+          <Route path="/detail" element={<DetailPage />} />
           <Route path="/setting" element={<SettingPage />} />
           <Route path="/fix" element={<FixPage />} />
         </Routes>
