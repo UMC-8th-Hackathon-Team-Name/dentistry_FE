b527e01aab365888610614e25a3e9ac9b2c5e530
2ssac <ivory.ma9ic@gmail.com>
2025-07-06 02:37:33 +0900

    전체 container h 높이 조정

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -16,7 +16,7 @@ import FixPage from "./pages/FixPage";
 
 function App() {
   return (
-    <div className="m-auto min-h-screen max-w-[412px] bg-[#FBFBFB] border-2 border-black">
+    <div className="m-auto max-h-[912px] max-w-[412px] bg-[#FBFBFB] border-2 border-black">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandigPage />} />
