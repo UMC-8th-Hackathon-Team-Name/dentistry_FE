96f2d242e41e7762960071197ad3ffb8a110f3ba
Minsu Kwon <kms37480@naver.com>
2025-07-06 04:21:03 +0900

    finish

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,14 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-<<<<<<< HEAD
-
 // auth
 import LandingPage from "./auth/LandingPage";
-import SignupTerms from "./auth/SignupTerms";
-=======
-// auth
-import LandingPage from "./auth/LandingPage";
->>>>>>> 759343dc1390a16130f3055511995d67d697a5a2
 import SignupEmail from "./auth/SignupEmail";
 import SignupPassword from "./auth/SignupPassword";
 import SignupComplete from "./auth/SignupComplete";
@@ -20,58 +13,37 @@ import DeleteAccount from "./auth/DeleteAccount";
 import DetailPage from "./pages/DetailPage";
 import SettingPage from "./pages/SettingPage";
 import FixPage from "./pages/FixPage";
-<<<<<<< HEAD
-=======
-
-import LandigPage from "./auth/LandingPage";
 import SignupTerms from "./auth/SignupTerms";
 
->>>>>>> 759343dc1390a16130f3055511995d67d697a5a2
 import Search from "./main/Search";
-import SearchDetailModal from "./main/SearchDetailModal";
-import LineProgressProps from "./components/LineProgress";
-import LineProgress from "./components/LineProgress";
 import Main2 from "./main/Main2";
 
 import MapPage from "./pages/MapPage";
+import Main from "./main/Main";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="m-auto max-h-[912px] max-w-[412px] bg-[#FBFBFB] border-2 border-black">
-=======
     <div className="m-auto h-[912px] max-w-[412px] bg-[#FBFBFB] border-2 border-black">
->>>>>>> 759343dc1390a16130f3055511995d67d697a5a2
       <BrowserRouter>
         <Routes>
           {/* 인증/회원가입 관련 */}
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup/terms" element={<SignupTerms />} />
-<<<<<<< HEAD
           <Route path="/" element={<LandingPage />} />
           <Route path="signup/terms" element={<SignupTerms />} />
           <Route path="/main" element={<Main />} />
-
-=======
-          <Route path="/" element={<LandigPage />} />
           <Route path="signup/terms" element={<SignupTerms />} />
-          <Route path="/main" element={<Main />} />
->>>>>>> 759343dc1390a16130f3055511995d67d697a5a2
           <Route path="/signup/email" element={<SignupEmail />} />
           <Route path="/signup/password" element={<SignupPassword />} />
           <Route path="/signup/complete" element={<SignupComplete />} />
           <Route path="/delete-account" element={<DeleteAccount />} />
           {/* 메인 및 기타 페이지 */}
-          <Route path="/main" element={<Main />} />
           <Route path="/main2" element={<Main2 />} />
           <Route path="/detail" element={<DetailPage />} />
           <Route path="/setting" element={<SettingPage />} />
           <Route path="/fix" element={<FixPage />} />
-<<<<<<< HEAD
-
           <Route path="/search" element={<Search />} />
-=======
           <Route path="/main/search" element={<Search />} />
           <Route path="/search" element={<Search />} />
           f57fc2e (feat:search페이지 구현)
@@ -79,7 +51,6 @@ function App() {
           <Route path="/setting" element={<SettingPage />} />
           <Route path="/fix" element={<FixPage />} />
           <Route path="/map" element={<MapPage />} />
->>>>>>> 759343dc1390a16130f3055511995d67d697a5a2
         </Routes>
       </BrowserRouter>
     </div>
