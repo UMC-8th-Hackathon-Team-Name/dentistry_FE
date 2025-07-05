98704abd826477b0a6ebff76a24a8356d3ce3c22
bini0918 <165636716+bini0918@users.noreply.github.com>
2025-07-05 22:41:12 +0900

    feat:detail/fix

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import LandigPage from "./auth/LandingPage";
 import Terms from "./auth/Terms";
 import Main from "./main/Main";
@@ -7,10 +8,14 @@ import SignupEmail from "./auth/SignupEmail";
 import SignupPassword from "./auth/SignupPassword";
 import SignupComplete from "./auth/SignupComplete";
 
+import DetailPage from "./pages/DetailPage";
+import SettingPage from "./pages/SettingPage";
+import FixPage from "./pages/FixPage";
+
 function App() {
   return (
-    <BrowserRouter>
-      <div className="m-auto min-h-screen max-w-[412px] bg-[#FBFBFB] border-2 border-black">
+    <div className="m-auto min-h-screen max-w-[412px] bg-[#FBFBFB] border-2 border-black">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandigPage />} />
           <Route path="/terms" element={<Terms />} />
@@ -18,9 +23,13 @@ function App() {
           <Route path="/signup/email" element={<SignupEmail />} />
           <Route path="/signup/password" element={<SignupPassword />} />
           <Route path="/signup/complete" element={<SignupComplete />} />
+
+          <Route path="/" element={<DetailPage />} />
+          <Route path="/setting" element={<SettingPage />} />
+          <Route path="/fix" element={<FixPage />} />
         </Routes>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
   );
 }
 
