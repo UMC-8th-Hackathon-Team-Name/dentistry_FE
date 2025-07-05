35a328ba6d5c34a76467a927c54ab6af5ec5911f
Minsu Kwon <kms37480@naver.com>
2025-07-05 17:19:21 +0900

    feat:로그인 버튼 구현

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Button from "./components/common/button";
 
 function App() {
   return (
     <>
       <div className="flex items-center justify-center w-screen h-screen text-5xl font-bold bg-amber-300">
         Dentist!
-        <Button text="동의하고 가입하기" />
       </div>
     </>
   );
