64570e6ce347f0ce5ec9e1c72f1c13d5f961882a
2ssac <ivory.ma9ic@gmail.com>
2025-07-06 04:36:33 +0900

    refactor/signup-complete

--- app/src/auth/SignupComplete.tsx
+++ app/src/auth/SignupComplete.tsx
@@ -9,41 +9,48 @@ const SignupComplete = () => {
   const nav = useNavigate();
 
   return (
-    <div className="relative flex flex-col items-center justify-end w-full h-screen gap-4">
+    <main
+      className="relative flex flex-col items-center justify-end w-full h-screen gap-4"
+      role="회원가입 완료 페이지"
+    >
       {/* 닫기 버튼 */}
-      <div>
-        <img
-          src={closeIcon}
-          alt="closeButton"
-          className="absolute w-6 h-6 cursor-pointer top-6 left-6"
-          onClick={() => {
-            nav("/");
-          }}
-        />
-      </div>
+      <button
+        onClick={() => nav("/")}
+        className="absolute top-6 left-6"
+        aria-label="닫기"
+        role="닫기 버튼"
+      >
+        <img src={closeIcon} alt="닫기 아이콘" className="w-6 h-6" />
+      </button>
 
+      {/* 프로그래스 바 */}
       <ProgressBar currentStep={3} totalSteps={3} />
 
-      <div className="flex flex-col w-full gap-4 px-8">
-        <div className="text-2xl font-semibold text-[#4C4C4C]">
+      {/* 안내 문구 영역 */}
+      <header className="flex flex-col w-full gap-4 px-8" role="가입 완료 안내">
+        <h1 className="text-2xl font-semibold text-[#4C4C4C]">
           회원 가입이 완료되었습니다!
-        </div>
-        <div className="text-base font-normal text-[#4C4C4C] mb-3">
+        </h1>
+        <p className="text-base font-normal text-[#4C4C4C] mb-3">
           이제 세부정보를 입력하러 가볼까요?
-        </div>
-      </div>
+        </p>
+      </header>
 
-      {/* Container */}
-      <div className="w-96 h-[630px] flex flex-col justify-end items-center pt-12 pb-20 px-7">
+      {/* 버튼 영역 */}
+      <section
+        className="w-96 h-[630px] flex flex-col justify-end items-center pt-12 pb-20 px-7"
+        role="로그인 이동 버튼 영역"
+      >
         <Button
           text={"로그인 하기"}
           type={"button"}
           onClick={() => {
             nav("/login");
           }}
+          role="로그인 버튼"
         />
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
