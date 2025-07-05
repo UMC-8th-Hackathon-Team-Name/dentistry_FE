e785a993890032713abd7bb50a0839f6a7f2a5cf
2ssac <ivory.ma9ic@gmail.com>
2025-07-05 15:11:30 +0900

    DoorooDooroo Front 초기 세팅

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center w-screen h-screen text-5xl font-bold bg-amber-300">
+        Dentist!
+      </div>
+    </>
+  );
+}
+
+export default App;
