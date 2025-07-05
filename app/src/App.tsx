4130de830a5901361841065c986e2355124801f8
2ssac <ivory.ma9ic@gmail.com>
2025-07-05 19:17:11 +0900

    feat:이용약관 화면 및 기능 구현 완료

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandigPage from "./auth/LandingPage";
+import Terms from "./auth/Terms";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="m-auto min-h-screen max-w-[412px] border-2 border-black">
+      <div className="m-auto min-h-screen max-w-[412px] bg-[#FBFBFB] border-2 border-black">
         <Routes>
           <Route path="/" element={<LandigPage />} />
+          <Route path="/terms" element={<Terms />} />
         </Routes>
       </div>
     </BrowserRouter>
