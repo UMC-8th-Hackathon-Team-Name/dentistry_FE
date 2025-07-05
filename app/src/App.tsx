e2be727b57daef559c2550a6570043038c482772
bini0918 <165636716+bini0918@users.noreply.github.com>
2025-07-05 22:41:12 +0900

    feat:detail/fix

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import DetailPage from "./pages/DetailPage";
+import SettingPage from "./pages/SettingPage";
+import FixPage from "./pages/FixPage";
 
 function App() {
   return (
-    <>
-      <div className="flex items-center justify-center w-screen h-screen text-5xl font-bold bg-amber-300">
-        Dentist!
+    <BrowserRouter>
+      <div className="m-auto min-h-screen max-w-[412px] border-2 border-black">
+        <Routes>
+          <Route path="/" element={<DetailPage />} />
+          <Route path="/setting" element={<SettingPage />} />
+          <Route path="/fix" element={<FixPage />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
